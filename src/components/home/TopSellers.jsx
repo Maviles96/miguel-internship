ac1db0a078bbc617737cc58233eaa7de9b118276
Miguel Aviles <avilesmiguel1996@yahoo.com>
2026-08-30 23:06:52 -0500

    YourMessage

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState} from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import { useEffect, useState } from "react";
 import Skeleton from 'react-loading-skeleton';
