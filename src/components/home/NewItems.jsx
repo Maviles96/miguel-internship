22d52d1b32fd194c23aa5db6999171a33c824564
Miguel Aviles <avilesmiguel1996@yahoo.com>
2026-09-07 20:11:26 -0500

    Fix NFT item route

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -67,7 +67,7 @@ fetchNewItems();
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item/${item.id}`}>
                     <img
                       src={item.nftImage}
                       className="lazy nft__item_preview"
@@ -76,7 +76,7 @@ fetchNewItems();
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item/${item.id}`}>
                     <h4>{item.title}</h4>
                   </Link>
                   <div className="nft__item_price">{item.price} ETH</div>
